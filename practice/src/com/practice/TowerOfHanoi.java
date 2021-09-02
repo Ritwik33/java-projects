6d7c6a6e925231c020b2c726aca128d6547c511c
Ritwik33 <ritwikawesome556@gmail.com>
2021-09-02 13:18:28 +0530

    Update TowerOfHanoi.java

--- practice/src/com/practice/TowerOfHanoi.java
+++ practice/src/com/practice/TowerOfHanoi.java
@@ -1,5 +1,7 @@
 package com.practice;
 
+import java.util.Scanner;
+
 public class TowerOfHanoi {
     public static void toh(int n, char beg, char aux, char end) {
         if(n == 1) {
@@ -11,7 +13,10 @@ public class TowerOfHanoi {
         toh(n-1, aux, beg, end);
     }
     public static void main(String[] args) {
-        int n = 3;
+        int n;
+        System.out.print("Enter value of n: ");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
         toh(n, 'A', 'B', 'C');
     }
 }
