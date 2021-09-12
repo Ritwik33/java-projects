bd7dd243439409aa185dfb589825c85833a28a00
Ritwik33 <ritwikawesome556@gmail.com>
2021-09-12 17:47:00 +0530

    Create binarySearch.java

---
+++ practice/src/com/practice/binarySearch.java
@@ -0,0 +1,10 @@
+package com.practice;
+
+import java.util.Arrays;
+
+public class binarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1,3,6,10};
+        System.out.println(Arrays.binarySearch(arr, 5));
+    }
+}
