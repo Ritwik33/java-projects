4ef7a80449a29402fa1d168fa7e72177ccbfc813
Ritwik33 <ritwikawesome556@gmail.com>
2021-09-17 01:39:31 +0530

    Update Gcdandlcm.java

--- practice/src/com/practice/Gcdandlcm.java
+++ practice/src/com/practice/Gcdandlcm.java
@@ -3,17 +3,17 @@ package com.practice;
 import java.util.Scanner;
 
 public class Gcdandlcm {
-    static long gc(long a, long b) {
+    int gcd(int a, int b) {
         if(b == 0) return a;
-        else return gc(b, a%b);
+        return gcd(b, a%b);
     }
+
     public static void main(String[] args) {
+        Gcdandlcm obj = new Gcdandlcm();
         Scanner sc = new Scanner(System.in);
-        long a, b;
+        int a, b;
         a = sc.nextInt();
         b = sc.nextInt();
-        long g = gc(a, b);
-        long l = (a*b)/g;
-        System.out.println(g + " " + l);
+        System.out.println(obj.gcd(a, b));
     }
 }
