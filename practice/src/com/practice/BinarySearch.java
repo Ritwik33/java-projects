51288344245f88d043bf864172531cb323099c48
Ritwik33 <ritwikawesome556@gmail.com>
2021-09-29 18:47:20 +0530

    Update BinarySearch.java

--- practice/src/com/practice/BinarySearch.java
+++ practice/src/com/practice/BinarySearch.java
@@ -2,7 +2,7 @@ package com.practice;
 
 import java.util.Arrays;
 
-public class binarySearch {
+public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {1,3,6,10};
         System.out.println(Arrays.binarySearch(arr, 5));
